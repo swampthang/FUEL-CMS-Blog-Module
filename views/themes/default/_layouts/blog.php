36eea7a332acc1f0a29ec2aa6b607eebfa6a2195
Carvel Avis <cavis@rhapsody.com>
2015-02-11 18:51:34 -0600

    Add meta tags

--- views/themes/default/_layouts/blog.php
+++ views/themes/default/_layouts/blog.php
@@ -6,7 +6,7 @@ if (isset($current_post) AND !$is_home)
 	fuel_set_var('canonical', $post->url);
 	if ($post->has_page_title()) fuel_set_var('page_title', $post->page_title);
 	if ($post->has_meta_description()) fuel_set_var('meta_description', $post->meta_description);
-	if ($post->has_meta_keywords()) fuel_set_var('meta_keywords', $post->meta_keywords);	
+	if ($post->has_meta_keywords()) fuel_set_var('meta_keywords', $post->meta_keywords);
 }
 ?>
 <?php $this->load->view('_blocks/header')?>
